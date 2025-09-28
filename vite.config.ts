cf61990514656f49d84f318bef8d2b68fc9749f4
Nnagarjuna55 <nagarjuna93900@gmail.com>
2025-09-28 11:49:14 +0530

    m

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,17 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       // "/api": "http://localhost:5000", // backend integration
+//       "/api": "https://booking-tour-465o.onrender.com", // backend integration
+//     },
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -7,8 +21,10 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      // "/api": "http://localhost:5000", // backend integration
       "/api": "https://booking-tour-465o.onrender.com", // backend integration
     },
+  },
+  build: {
+    outDir: "build", // <-- Change output folder to "build" for Vercel
   },
 });
