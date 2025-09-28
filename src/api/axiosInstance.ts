a1ab250d01d72cfabdc77890a2e9521414f26b25
Nnagarjuna55 <nagarjuna93900@gmail.com>
2025-09-28 15:20:29 +0530

    m

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,26 @@
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   https:"//booking-tour-backend-mlgs.onrender.com": "/api", // proxied to backend
+//   // baseURL: "/api", // proxied to backend
+//   headers: { "Content-Type": "application/json" },
+// });
+
+// // attach token if available
+// axiosInstance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default axiosInstance;
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/api", // proxied to backend
+  baseURL: "https://booking-tour-backend-mlgs.onrender.com/api", // ✅ backend base URL
+  headers: { "Content-Type": "application/json" },
 });
 
 // attach token if available
