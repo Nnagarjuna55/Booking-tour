86722e66aeacd0d05195e7bd40d6389889f07de1
Nnagarjuna55 <nagarjuna93900@gmail.com>
2025-09-28 11:47:22 +0530

    m

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "/api": "http://localhost:5000", // backend integration
+      // "/api": "http://localhost:5000", // backend integration
+      "/api": "https://booking-tour-465o.onrender.com", // backend integration
     },
   },
 });
